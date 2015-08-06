29dac1e842ec6635b977639cacae4c9c147dc4ed
Grégoire Audoux <projects@gregoire-audoux.fr>
2015-08-06 20:05:55 +0200

    Un peut de gravity sur les joueurs

--- app/Player/index.js
+++ app/Player/index.js
@@ -3,8 +3,18 @@ import './assets';
 
 console.log('Player loaded');
 
-export default class Player {
+export
+default class Player {
   constructor(color = 'red', x = 100, y = 100) {
     this.sprite = game.add.sprite(x, y, `player:${color}`);
+
+
+    //  We need to enable physics on the player
+    game.physics.arcade.enable(this.sprite);
+
+    //  Player physics properties. Give the little guy a slight bounce.
+    this.sprite.body.bounce.y = 0.2;
+    this.sprite.body.gravity.y = 300;
+    this.sprite.body.collideWorldBounds = true
   }
 }
