699add56f36997b689194f80b370e55e9186c918
Grégoire Audoux <projects@gregoire-audoux.fr>
2015-08-07 20:51:19 +0200

    Basic player movements

--- app/Player/index.js
+++ app/Player/index.js
@@ -1,8 +1,12 @@
 import {game} from '../game';
 import './assets';
 
+const MOVE_LEFT = -150;
+const MOVE_RIGHT = 150;
+
 export
 default class Player {
+
   constructor(color = 'red', x = 100, y = 100) {
     this.sprite = game.add.sprite(x, y, `player:${color}`);
 
@@ -13,5 +17,24 @@ default class Player {
     //  Player physics properties. Give the little guy a slight bounce.
     this.sprite.body.bounce.y = 0;
     this.sprite.body.collideWorldBounds = true
+  }
+
+  update(left, right, up) {
+    console.log(up, this.sprite.body.touching.down)
+
+
+    this.sprite.body.velocity.x = 0;
+
+    if (left && !right) {
+      this.sprite.body.velocity.x = MOVE_LEFT;
+    }
+    else if (right && !left) {
+      this.sprite.body.velocity.x = MOVE_RIGHT;
+    }
+
+    if (up && this.sprite.body.blocked.down) {
+
+      this.sprite.body.velocity.y = -350;
+    }
   }
 }
